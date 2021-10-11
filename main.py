fb9d638e5e2b2a207951ffe3f2c3c9e6114e42df
ericlove02 <53005525+ericlove02@users.noreply.github.com>
2021-10-11 11:07:20 -0500

    used win32 to speed up screenshot fps

--- main.py
+++ main.py
@@ -8,6 +8,30 @@ import numpy as np
 import pyautogui as pag
 from time import time
 import keyboard
+from PIL import ImageGrab
+import win32gui, win32ui, win32con
+
+
+def window_capture:
+    w = 1920  # set this
+    h = 1080  # set this
+    bmpfilenamename = "out.bmp"  # set this
+
+    hwnd = win32gui.FindWindow(None, windowname)
+    wDC = win32gui.GetWindowDC(hwnd)
+    dcObj = win32ui.CreateDCFromHandle(wDC)
+    cDC = dcObj.CreateCompatibleDC()
+    dataBitMap = win32ui.CreateBitmap()
+    dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+    cDC.SelectObject(dataBitMap)
+    cDC.BitBlt((0, 0), (w, h), dcObj, (0, 0), win32con.SRCCOPY)
+    dataBitMap.SaveBitmapFile(cDC, bmpfilenamename)
+
+    # Free Resources
+    dcObj.DeleteDC()
+    cDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, wDC)
+    win32gui.DeleteObject(dataBitMap.GetHandle())
 
 
 def click_head(mouseX, mouseY):
@@ -18,7 +42,7 @@ def click_head(mouseX, mouseY):
 fps_time = time()
 while True:
 
-    screenshot = pag.screenshot()
+    screenshot = ImageGrab.grab()
     screenshot = np.array(screenshot)
     screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
 
