11156ed4f94696837115139e9d0cc342bfd0a84d
ericlove02 <53005525+ericlove02@users.noreply.github.com>
2021-10-11 11:00:09 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+######################################################
+# Valorant Aimbot project using opencv and pyautogui #
+# Eric Love                                          #
+######################################################
+
+import cv2 as cv
+import numpy as np
+import pyautogui as pag
+from time import time
+import keyboard
+
+
+def click_head(mouseX, mouseY):
+    """use x,y coordinate of detected bots position and move the mouse and click"""
+    print("click")
+
+
+fps_time = time()
+while True:
+
+    screenshot = pag.screenshot()
+    screenshot = np.array(screenshot)
+    screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
+
+    cv.imshow('screenshot', screenshot)
+
+    print('FPS {}'.format(1 / (time() - fps_time)))
+    fps_time = time()
+
+    if cv.waitKey(1) == ord('`'):
+        cv.destroyAllWindows()
+        break
+
+print("done")
