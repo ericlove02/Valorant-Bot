8818cf5973267dc2a42993a2edcc517092f920f1
ericlove02 <53005525+ericlove02@users.noreply.github.com>
2021-10-11 18:51:05 -0500

    needs threshold adjustments and mouse movement

--- main.py
+++ main.py
@@ -3,19 +3,25 @@
 # Eric Love                                          #
 ######################################################
 
-from time import time
+from time import time, sleep
 
 import cv2 as cv
+import keyboard
+import mouse
 import numpy as np
 import pyautogui as pag
 import win32con
 import win32gui
 import win32ui
+import imutils
+from PIL import ImageGrab
 
 from windowcapture import WindowCapture
 
-EXIT_KEY = '`'
 TOGGLE_KEY = 'z'
+EXIT_KEY = '`'
+
+body_classifier = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_fullbody.xml')
 
 
 def get_screenshot():
@@ -23,7 +29,7 @@ def get_screenshot():
     w, h = 1920, 1080
 
     # for now we will set hwnd to None to capture the primary monitor
-    #hwnd = win32gui.FindWindow(None, window_name)
+    # hwnd = win32gui.FindWindow(None, window_name)
     hwnd = None
 
     # get the window image data
@@ -47,7 +53,7 @@ def get_screenshot():
     win32gui.DeleteObject(dataBitMap.GetHandle())
 
     # drop the alpha channel to work with cv.matchTemplate()
-    img = img[...,:3]
+    img = img[..., :3]
 
     # make image C_CONTIGUOUS to avoid errors with cv.rectangle()
     img = np.ascontiguousarray(img)
@@ -55,46 +61,85 @@ def get_screenshot():
     return img
 
 
-def click_head(mouseX, mouseY):
-    """use x,y coordinate of detected bots position and move the mouse and click"""
-    # pag.move(mouseX, mouseY)
-    pag.click()
+def red_mask_image(img):
+    img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-
-# wincap = WindowCapture('(107) Fast Window Capture - OpenCV Object Detection in Games #4 - YouTube - Google Chrome')
-
-fps_time = time()
-while True:
-    # get screenshots
-    screenshot = get_screenshot()
-    screenshot = np.array(screenshot)
-
-    # masks
-    img_hsv = cv.cvtColor(screenshot, cv.COLOR_BGR2HSV)
-
-    lower_red = np.array([0, 50, 50])
+    lower_red = np.array([0, 100, 20])
     upper_red = np.array([10, 255, 255])
     mask0 = cv.inRange(img_hsv, lower_red, upper_red)
 
-    lower_red = np.array([170, 50, 50])
-    upper_red = np.array([180, 255, 255])
+    lower_red = np.array([160, 100, 20])
+    upper_red = np.array([179, 255, 255])
     mask1 = cv.inRange(img_hsv, lower_red, upper_red)
 
     mask = mask0 + mask1
 
-    output_img = screenshot.copy()
+    output_img = img.copy()
     output_img[np.where(mask == 0)] = 0
 
     output_hsv = img_hsv.copy()
     output_hsv[np.where(mask == 0)] = 0
 
-    cv.imshow('screenshot', output_hsv)
+    return output_hsv
 
-    print('FPS {}'.format(1 / (time() - fps_time)))
-    fps_time = time()
+
+def detect_players(img):
+    hog = cv.HOGDescriptor()
+    hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
+
+    img = imutils.resize(img, width=min(1080, img.shape[1]))
+
+    (players, _) = hog.detectMultiScale(img,
+                                        winStride=(5, 5),
+                                        padding=(3, 3),
+                                        scale=1.21)
+    return players
+
+
+def click_head(mouse_x, mouse_y):
+    """use x,y coordinate of detected bots position and move the mouse and click"""
+    mouse.move(mouse_x, mouse_y, duration=0.03)
+    mouse.click(button="left")
+    print("Mouse clicked\n")
+
+
+fps_time = time()
+botOn = True
+while True:
+    if botOn:
+        screenshot = get_screenshot()
+        screenshot = np.array(screenshot)
+        screenshot = cv.GaussianBlur(screenshot, (5, 5), 0)
+        red_masked_screenshot = red_mask_image(screenshot)
+
+        gray = cv.cvtColor(red_masked_screenshot, cv.COLOR_BGR2GRAY)
+        detectedPlayers = body_classifier.detectMultiScale(red_masked_screenshot, 1.1, 0)
+
+        # detectedPlayers = detect_players(red_masked_screenshot)
+
+        for (x, y, w, h) in detectedPlayers:
+            cv.rectangle(red_masked_screenshot, (x, y),
+                         (x + w, y + h),
+                         (0, 0, 255), 2)
+            click_head(x, y)
+
+        cv.imshow('screenshot', red_masked_screenshot)
+
+        print('FPS {}'.format(1 / (time() - fps_time)))
+        fps_time = time()
+
+        if keyboard.is_pressed(TOGGLE_KEY):
+            botOn = False
+            print("Bot toggled off\n")
+            sleep(0.1)
+    elif not botOn:
+        botOn = False
+        if keyboard.is_pressed(TOGGLE_KEY):
+            botOn = True
+            print("Bot toggled on\n")
+            sleep(0.05)
 
     if cv.waitKey(1) == ord(EXIT_KEY):
+        print("Exiting...")
         cv.destroyAllWindows()
-        break
-
-print("done")
+        exit(0)
